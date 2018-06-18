e50c88720703d90a8cf292983dd31ecd859053ef
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-18 11:05:32 +0200

    Added Exercise 2

---
+++ week-06/day-01/AverageOfOddNumbers/AverageOfOddNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageOfOddNumbers
+{
+    class Program
+    {
+        //Write a LINQ Expression to get the average value of the odd numbers from the following array:
+
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var averageWithQuery = (from num in n
+                                    where num % 2 != 0
+                                    select num).Average();
+            var averageWithMethod = n.Where(num => num % 2 != 0).Average();
+            Console.WriteLine($"Average with query: {averageWithQuery}");
+            Console.WriteLine($"Average with method: {averageWithMethod}");
+            Console.ReadLine();
+        }
+    }
+}
