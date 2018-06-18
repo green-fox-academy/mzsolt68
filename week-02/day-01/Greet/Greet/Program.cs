2d88b7ebb8bb64ce54404f36e01cf888c3a5f625
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-18 17:08:30 +0200

    Added project and README Added Greet method

---
+++ week-02/day-01/Greet/Greet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a1 = "Greenfox";
+            Greet(a1);
+            Console.ReadKey();
+        }
+
+        static void Greet(string name)
+        {
+            Console.WriteLine($"Greetings dear, {name}");
+        }
+    }
+}
