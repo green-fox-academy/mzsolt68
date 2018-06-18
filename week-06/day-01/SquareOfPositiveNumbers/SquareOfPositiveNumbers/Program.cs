385588863c7e9ba61cbb73692296cbf6ff6c875b
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-18 11:23:08 +0200

    Added exercise 3

---
+++ week-06/day-01/SquareOfPositiveNumbers/SquareOfPositiveNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareOfPositiveNumbers
+{
+    class Program
+    {
+        //Write a LINQ Expression to get the squared value of the positive numbers from the following array:
+
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var squaredWithQuery = from num in n
+                                   where num > 0
+                                   select num * num;
+            var squaredWithMethod = n.Where(num => num > 0).Select(num => num * num);
+
+            Console.WriteLine("Squared with query:");
+            foreach (var num in squaredWithQuery)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine("\nSquared with method:");
+            foreach (var num in squaredWithMethod)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-06/day-01/SquareOfPositiveNumbers/SquareOfPositiveNumbers/readme.md
@@ -0,0 +1,3 @@
+﻿Write a LINQ Expression to get the squared value of the positive numbers from the following array:
+
+int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 }; 
