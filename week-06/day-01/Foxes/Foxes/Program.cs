c448d11ea1ef0c9dc3c03403747d62a45a101180
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-18 12:32:41 +0200

    Added queries and print of results

--- week-06/day-01/Foxes/Foxes/Program.cs
+++ week-06/day-01/Foxes/Foxes/Program.cs
@@ -10,6 +10,30 @@ namespace Foxes
     {
         static void Main(string[] args)
         {
+            List<Fox> foxes = new List<Fox>
+            {
+                new Fox { Name = "Vuk", Type = "Vulpes vulpes", Color = "Red" },
+                new Fox { Name = "Zelda", Type = "Vulpes corsac", Color = "Grey" },
+                new Fox { Name = "Karak", Type = "Vulpes pallida", Color = "Green" },
+                new Fox { Name = "Vadim", Type = "Vulpes zerda", Color = "Yellow" },
+                new Fox { Name = "Fred", Type = "Vulpes pallida", Color = "Brown" },
+                new Fox {Name = "Ginger", Type ="Vulpes zerda", Color = "Green" }
+            };
+
+            var greenFoxes = foxes.Where(fox => fox.Color == "Green");
+            var greenPallidas = foxes.Where(fox => fox.Type.Contains("pallida") && fox.Color == "Green");
+
+            Console.WriteLine("Green foxes:");
+            foreach (var fox in greenFoxes)
+            {
+                Console.WriteLine($"{fox.Name} color is {fox.Color}");
+            }
+            Console.WriteLine("\nGreen pallidas:");
+            foreach (var fox in greenPallidas)
+            {
+                Console.WriteLine($"{fox.Name} type is {fox.Type} and has color of {fox.Color}");
+            }
+            Console.ReadLine();
         }
     }
 }
