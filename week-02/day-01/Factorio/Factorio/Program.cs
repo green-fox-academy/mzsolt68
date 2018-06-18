03331ad66c7b50e5cdd406c5dc19cca1591c9ba7
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-18 17:21:17 +0200

    Added Factorio method

--- week-02/day-01/Factorio/Factorio/Program.cs
+++ week-02/day-01/Factorio/Factorio/Program.cs
@@ -10,6 +10,17 @@ namespace Factorio
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Factorio(6));
+
+            Console.ReadKey();
+        }
+
+        static int Factorio(int number)
+        {
+            int tmp = 1;
+            for (int i = 1; i <= number; i++)
+                tmp *= i;
+            return tmp;
         }
     }
 }
