200adcb5bec7ec709ff164fe279123af233bafd1
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-18 11:50:57 +0200

    Added Exercise 5

---
+++ week-06/day-01/FrequencyOfNumbers/FrequencyOfNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequencyOfNumbers
+{
+    class Program
+    {
+        //Write a LINQ Expression to find the frequency of numbers in the following array:
+
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+            var frequencyWithQuery = from number in n
+                                     group number by number into nums
+                                     orderby nums.Key
+                                     select new { nums.Key, Count = nums.Count() };
+            var frequencyWithMethod = n.GroupBy(num => num).OrderBy(grp => grp.Key);
+
+            Console.WriteLine("Frequency with query:");
+            foreach (var obj in frequencyWithQuery)
+            {
+                Console.WriteLine($"{obj.Key}: {obj.Count}");
+            }
+            Console.WriteLine("\nFrequency with method:");
+            foreach (var obj in frequencyWithMethod)
+            {
+                Console.WriteLine($"{obj.Key}: {obj.Count()}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-06/day-01/FrequencyOfNumbers/FrequencyOfNumbers/readme.md
@@ -0,0 +1,3 @@
+﻿Write a LINQ Expression to find the frequency of numbers in the following array:
+
+int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
