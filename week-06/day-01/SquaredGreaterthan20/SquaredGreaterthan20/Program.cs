be0b926c52341e55142af55dd6eb52459393e3d8
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-18 11:31:48 +0200

    Added Exercise 5

---
+++ week-06/day-01/SquaredGreaterthan20/SquaredGreaterthan20/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquaredGreaterthan20
+{
+    class Program
+    {
+        //Write a LINQ Expression to find which number squared value is more then 20 from the following array:
+
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var greaterWithQuery = from num in n
+                                   where num * num > 20
+                                   select num;
+            var greaterWithMethod = n.Where(num => num * num > 20);
+            Console.WriteLine("With query:");
+            foreach (var num in greaterWithQuery)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine("\nWith method:");
+            foreach (var num in greaterWithMethod)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-06/day-01/SquaredGreaterthan20/SquaredGreaterthan20/readme.md
@@ -0,0 +1,3 @@
+﻿Write a LINQ Expression to find which number squared value is more then 20 from the following array:
+
+int[] n = new[] { 3, 9, 2, 8, 6, 5 };
