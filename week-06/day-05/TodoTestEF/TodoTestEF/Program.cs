a3bc4e9c9f99f95447dc4dcde9d369c99af6d439
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-22 13:23:31 +0200

    Creating new test project for weekly demo

---
+++ week-06/day-05/TodoTestEF/TodoTestEF/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoTestEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Todos> todos = new List<Todos>();
+            using (TodoEntities db = new TodoTestEF.TodoEntities())
+            {
+                todos = db.Todos.ToList();
+            }
+            todos.ForEach(
+                todo => Console.WriteLine(todo.text));
+
+            Console.ReadKey();
+        }
+    }
+}
