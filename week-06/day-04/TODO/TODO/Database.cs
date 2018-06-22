f328fe16f450017d28727fe13ea1c12583d4209f
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-22 21:30:31 +0200

    Added new class for database manipulation, so it will be independent from the main program.

---
+++ week-06/day-04/TODO/TODO/Database.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace TODO
+{
+    class Database
+    {
+        public SQLiteConnection connection;
+
+        public Database(string database)
+        {
+            connection = new SQLiteConnection($"Data Source={database}, Version 3");
+            if (!File.Exists(database))
+                SQLiteConnection.CreateFile(database);
+            string createTable = "CREATE TABLE Todos(id INTEGER PRIMARY KEY AUTOINCREMENT, text VARCHAR NOT NULL, createdAt DATETIME NOT NULL, completedAt DATETIME)";
+            using (connection)
+            {
+                connection.Open();
+                using (SQLiteCommand command = new SQLiteCommand(createTable, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+
+        public void OpenConnection()
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (connection.State != System.Data.ConnectionState.Closed)
+                connection.Close();
+        }
+    }
+}
