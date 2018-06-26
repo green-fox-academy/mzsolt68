42467f01391371f1c7b695927082e50732a4e754
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-26 12:21:17 +0200

    New Endpoint and View added for Simba account class

---
+++ week-07/day-02/BankOfSimba/src/BankOfSimba/Controllers/SimbaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BankOfSimba.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BankOfSimba.Controllers
+{
+    public class SimbaController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            BankAccount account = new BankAccount()
+            {
+                Name = "Simba",
+                Balance = 2000,
+                AnimalType = BankAccount.Animal.Lion
+            };
+            return View(account);
+        }
+    }
+}
