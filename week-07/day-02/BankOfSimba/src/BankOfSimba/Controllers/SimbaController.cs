592e536fe95b5a0f704c1a0e36016d5a057c5512
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-26 14:20:40 +0200

    Added new ViewModel, accountlist view and endpoint

--- week-07/day-02/BankOfSimba/src/BankOfSimba/Controllers/SimbaController.cs
+++ week-07/day-02/BankOfSimba/src/BankOfSimba/Controllers/SimbaController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BankOfSimba.Models;
+using BankOfSimba.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,6 +12,18 @@ namespace BankOfSimba.Controllers
 {
     public class SimbaController : Controller
     {
+        static SimbaIndexViewModel accountViewModel = new SimbaIndexViewModel()
+        {
+            AccountList = new List<BankAccount>()
+            {
+                new BankAccount { Name = "Rafiki", Balance = 300, AnimalType = BankAccount.Animal.Monkey },
+                new BankAccount { Name = "Pumba", Balance = 4000, AnimalType = BankAccount.Animal.Warthog },
+                new BankAccount { Name = "Timon", Balance = 2400, AnimalType = BankAccount.Animal.Meerkats },
+                new BankAccount { Name = "Sarafina", Balance = 9000, AnimalType = BankAccount.Animal.Lion },
+                new BankAccount { Name = "Nala", Balance = 980, AnimalType = BankAccount.Animal.Lion }
+            }
+        };
+
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -21,6 +34,12 @@ namespace BankOfSimba.Controllers
                 AnimalType = BankAccount.Animal.Lion
             };
             return View(account);
+        }
+
+        public IActionResult AccountList()
+        {
+
+            return View(accountViewModel);
         }
     }
 }
