856ed9e020840ba4ff0ec0b87be9ebc6b95c9818
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-26 12:09:35 +0200

    BankAccount class created

---
+++ week-07/day-02/BankOfSimba/src/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public enum Animal
+        {
+            Lion, Monkey, Warthog, Meerkats, Zebra
+        }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public Animal AnimalType { get; set; }
+    }
+}
