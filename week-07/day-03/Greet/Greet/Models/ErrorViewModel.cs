8f9230c3b5f8af26b56e6c3082a8aaad657ca517
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-27 13:25:03 +0200

    Application created

---
+++ week-07/day-03/Greet/Greet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Greet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
