f98d51b646627e3e0b2bc8b2f35d7629b310178d
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-27 15:31:53 +0200

    Added Todo repository abstract class and its implementation

---
+++ week-07/day-03/TodoMVC/TodoMVC/Services/TodoRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoMVC.Models;
+
+namespace TodoMVC.Services
+{
+    public abstract class TodoRepo
+    {
+        public abstract List<Todo> GetTodos();
+        public abstract void AddTodo(Todo task);
+        public abstract void DeleteTodo(Todo task);
+    }
+}
---
+++ week-07/day-03/TodoMVC/TodoMVC/Services/TodoWithList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoMVC.Models;
+
+namespace TodoMVC.Services
+{
+    public class TodoWithList : TodoRepo
+    {
+        private List<Todo> taskList;
+
+        public TodoWithList()
+        {
+            taskList = new List<Todo>
+            {
+                new Todo{ID = 1, Text = "Feed the dog", Created = DateTime.Parse("2018-06-27")},
+                new Todo{ID = 2, Text = "Clean the garage", Created = DateTime.Parse("2018-05-31"), Completed = DateTime.Parse("2018.06.25")}
+            };
+        }
+
+        public override void AddTodo(Todo task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteTodo(Todo task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Todo> GetTodos()
+        {
+            return taskList;
+        }
+    }
+}
