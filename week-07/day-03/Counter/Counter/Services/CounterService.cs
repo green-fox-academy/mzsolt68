780ed3040c642af20d87276622511330f8e99ff9
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-27 12:03:14 +0200

    Counter service added

---
+++ week-07/day-03/Counter/Counter/Services/CounterService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Services
+{
+    public class CounterService : ICounter
+    {
+        private int Clicks = 0;
+
+        public int GetNUmber()
+        {
+            return Clicks;
+        }
+
+        public void Increase()
+        {
+            Clicks++;
+        }
+    }
+}
