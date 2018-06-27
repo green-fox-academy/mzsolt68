b7c85e2e726792f375a3c09facc5610349ec31c0
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-27 13:40:05 +0200

    INdex view added to name input

---
+++ week-07/day-03/Greet/Greet/Controllers/GreetingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Greet.Controllers
+{
+    public class GreetingController : Controller
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/")]
+        public IActionResult Greet()
+        {
+            return RedirectToAction("Greeting");
+        }
+
+        [HttpGet]
+        [Route("greet")]
+        public IActionResult Greeting()
+        {
+            return View();
+        }
+    }
+}
