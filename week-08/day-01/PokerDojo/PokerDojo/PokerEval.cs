2bda002555a3fef75dc76bc79452f868b000f00a
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-02 12:26:43 +0200

    PokerDojo: Added same suit counting method

--- week-08/day-01/PokerDojo/PokerDojo/PokerEval.cs
+++ week-08/day-01/PokerDojo/PokerDojo/PokerEval.cs
@@ -8,19 +8,24 @@ namespace PokerDojo
 {
     public class PokerEval
     {
-        public enum Rank : int {T = 10, J, Q, K, A  };
+        public enum Rank : int { T = 10, J, Q, K, A  };
         public string Evaluate(string[] black, string[] white)
         {
             if ( (GroupByRank(black, 5) != 0) || (GroupByRank(white, 5) != 0))
                 return "Hiba";
             if (HasDuplicates(black, white))
                 return "Hiba";
-            return "black";
+            return "";
         }
 
         int GroupByRank(string[] hand, int count)
         {
             return hand.GroupBy(c => c[0]).Count(g => g.Count() == count);
+        }
+
+        int GroupBySuit(string[] hand, int count)
+        {
+            return hand.GroupBy(c => c[1]).Count(g=>g.Count() == count);
         }
 
         bool HasDuplicates(string[] hand1, string[] hand2)
