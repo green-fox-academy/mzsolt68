b90515746cd9e65aac3187513da4efa9a29f0f6f
Mlatilik Zsolt <mlatilik.zsolt@no-reply.github.com>
2018-07-03 14:15:11 +0200

    Todo app with EF: TodoController and View added to project. Todocontroller modified, mapped / and /list routes to controller

---
+++ week-08/day-02/TodoWithEF/TodoWithEF/Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoWithEF.Controllers
+{
+    [Route("/")]
+    [Route("/list")]
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
