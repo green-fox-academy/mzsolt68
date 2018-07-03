0c9e97a8712b1f2638ec0c5463919871af30ce10
Mlatilik Zsolt <mlatilik.zsolt@no-reply.github.com>
2018-07-03 14:36:30 +0200

    Todo app with EF: Repository injected to the controller

--- week-08/day-02/TodoWithEF/TodoWithEF/Controllers/TodoController.cs
+++ week-08/day-02/TodoWithEF/TodoWithEF/Controllers/TodoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TodoWithEF.Services;
 
 namespace TodoWithEF.Controllers
 {
@@ -10,7 +11,12 @@ namespace TodoWithEF.Controllers
     [Route("/list")]
     public class TodoController : Controller
     {
+        TodoRepository todoRepository;
 
+        public TodoController(TodoRepository repository)
+        {
+            todoRepository = repository;
+        }
 
         public IActionResult Index()
         {
