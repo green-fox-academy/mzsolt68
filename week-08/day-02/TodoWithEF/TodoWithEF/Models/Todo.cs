316b7d000d02182704926fd7c10dea26c41784e4
Mlatilik Zsolt <mlatilik.zsolt@no-reply.github.com>
2018-07-03 14:02:55 +0200

    Todo app with EF: Todo class created

---
+++ week-08/day-02/TodoWithEF/TodoWithEF/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoWithEF.Models
+{
+    public class Todo
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        [DefaultValue(false)]
+        public bool IsUrgent { get; set; }
+        [DefaultValue(false)]
+        public bool IsDone { get; set; }
+    }
+}
