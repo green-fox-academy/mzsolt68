5946427d4579b802c712b163d581932ab94ef894
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-05 14:20:33 +0200

    add (repository): add Assignee repository and interface

---
+++ week-08/day-02/TodoWithEF/TodoWithEF/Services/AssigneeRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoWithEF.Models;
+using TodoWithEF.Repositories;
+
+namespace TodoWithEF.Services
+{
+    public class AssigneeRepository : IAssigneeRepository
+    {
+        private TodoContext todoContext;
+
+        public AssigneeRepository(TodoContext context)
+        {
+            todoContext = context;
+        }
+
+        public void AddAssignee(Assignee newAssignee)
+        {
+            todoContext.Assignees.Add(newAssignee);
+            todoContext.SaveChanges();
+        }
+
+        public void DeleteAssignee(int id)
+        {
+            todoContext.Assignees.Remove(GetAssignee(id));
+            todoContext.SaveChanges();
+        }
+
+        public Assignee GetAssignee(int id)
+        {
+            return todoContext.Assignees.Where(a => a.ID == id).SingleOrDefault();
+        }
+
+        public List<Assignee> ListAllAssignees()
+        {
+            return todoContext.Assignees.ToList();
+        }
+
+        public void UpdateAssignee(Assignee updatedAssignee)
+        {
+            todoContext.Assignees.Update(updatedAssignee);
+            todoContext.SaveChanges();
+        }
+    }
+}
---
+++ week-08/day-02/TodoWithEF/TodoWithEF/Services/IAssigneeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoWithEF.Models;
+
+namespace TodoWithEF.Services
+{
+    interface IAssigneeRepository
+    {
+        List<Assignee> ListAllAssignees();
+        void AddAssignee(Assignee newAssignee);
+        void DeleteAssignee(int id);
+        void UpdateAssignee(Assignee updatedAssignee);
+        Assignee GetAssignee(int id);
+    }
+}
