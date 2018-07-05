50f5c27575adcfd8d48a00c0dd448792f4bd96d6
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-05 14:22:09 +0200

    update (repository): update todorepository to use correct database table

--- week-08/day-02/TodoWithEF/TodoWithEF/Services/TodoRepository.cs
+++ week-08/day-02/TodoWithEF/TodoWithEF/Services/TodoRepository.cs
@@ -23,7 +23,7 @@ namespace TodoWithEF.Services
 
         public void AddTodo(Todo newTodo)
         {
-            todoContext.Add(newTodo);
+            todoContext.Todos.Add(newTodo);
             todoContext.SaveChanges();
         }
 
@@ -34,13 +34,13 @@ namespace TodoWithEF.Services
 
         public void DeleteTodo(int ID)
         {
-            todoContext.Remove(todoContext.Todos.Where(t => t.Id == ID).SingleOrDefault());
+            todoContext.Todos.Remove(todoContext.Todos.Where(t => t.Id == ID).SingleOrDefault());
             todoContext.SaveChanges();
         }
 
         public void UpdateTodo(Todo updatedTodo)
         {
-            todoContext.Update(updatedTodo);
+            todoContext.Todos.Update(updatedTodo);
             todoContext.SaveChanges();
         }
 
