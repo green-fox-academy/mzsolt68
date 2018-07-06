645159f71a54900bf5f80209272587180403f7f5
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-06 11:26:15 +0200

    add (view): add post list view

--- week-08/day-05/CloneReddit/CloneReddit/Controllers/HomeController.cs
+++ week-08/day-05/CloneReddit/CloneReddit/Controllers/HomeController.cs
@@ -5,33 +5,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CloneReddit.Models;
+using CloneReddit.Services;
 
 namespace CloneReddit.Controllers
 {
     public class HomeController : Controller
     {
+        private IRedditRepo redditRepository;
+
+        public HomeController(IRedditRepo repo)
+        {
+            redditRepository = repo;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(redditRepository.GetAllPost());
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
