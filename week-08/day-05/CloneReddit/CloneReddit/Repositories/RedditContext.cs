9ed0292433d460bbfa2b281795f18b70a97132be
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-06 10:33:57 +0200

    add (model): datamodel and context

---
+++ week-08/day-05/CloneReddit/CloneReddit/Repositories/RedditContext.cs
@@ -0,0 +1,19 @@
+﻿using CloneReddit.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloneReddit.Repositories
+{
+    public class RedditContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public RedditContext(DbContextOptions<RedditContext> options) : base(options)
+        {
+
+        }
+    }
+}
