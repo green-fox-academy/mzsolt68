c45a2834f5513c156b207ad1f15efe4d6e888b86
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-09 12:27:23 +0200

    add (controller): add HomeController

---
+++ week-09/day-01/RESTApiTest/RESTApiTest/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTApiTest.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Home")]
+    public class HomeController : Controller
+    {
+
+    }
+}
