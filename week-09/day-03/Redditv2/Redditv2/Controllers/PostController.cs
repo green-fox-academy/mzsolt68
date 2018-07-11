4a9ec6ac7d2a456d7259b25a278eb3bc569ac539
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-11 10:52:09 +0200

    ad (controller): add PostController and default endpoint

---
+++ week-09/day-03/Redditv2/Redditv2/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Redditv2.Services;
+
+namespace Redditv2.Controllers
+{
+    [Produces("application/json")]
+    [Route("/posts")]
+    public class PostController : Controller
+    {
+        private IPostRepository postRepository;
+
+        public PostController(IPostRepository repository)
+        {
+            postRepository = repository;
+        }
+
+        [HttpGet("/posts")]
+        public IActionResult GetPosts()
+        {
+            return Json(new { posts = postRepository.GetPosts() });
+        }
+    }
+}
