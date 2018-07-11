cab5c88028ad78bb83d4d3736e3a40e4928827a4
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-11 10:12:56 +0200

    add (context): add dbcontext

---
+++ week-09/day-03/Redditv2/Redditv2/Repositories/RedditDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Redditv2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Redditv2.Repositories
+{
+    public class RedditDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public RedditDbContext(DbContextOptions<RedditDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
