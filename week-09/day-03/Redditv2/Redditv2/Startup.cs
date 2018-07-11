6345c3c28cafb1589fd6cac2fd1afc9a69fce715
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-11 11:54:46 +0200

    update (service): add repository to services

--- week-09/day-03/Redditv2/Redditv2/Startup.cs
+++ week-09/day-03/Redditv2/Redditv2/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Redditv2.Repositories;
+using Redditv2.Services;
 
 namespace Redditv2
 {
@@ -27,6 +28,7 @@ namespace Redditv2
         {
             services.AddMvc();
             services.AddDbContext<RedditDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("RedditDatabase")));
+            services.AddTransient<IPostRepository, PostRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
