da9579667dd24514ae6dca6fae8f7d034b7c1dc0
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-11 10:20:22 +0200

    add (service): add connection string to appsettings.json register dbcontext in services

--- week-09/day-03/Redditv2/Redditv2/Startup.cs
+++ week-09/day-03/Redditv2/Redditv2/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Redditv2.Repositories;
 
 namespace Redditv2
 {
@@ -24,6 +26,7 @@ namespace Redditv2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<RedditDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("RedditDatabase")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
