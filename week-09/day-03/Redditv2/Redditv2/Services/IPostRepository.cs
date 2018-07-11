863f492eabda8d03aaa5a8f39395407cd1674715
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-11 10:38:41 +0200

    add (service): add PostRepository

---
+++ week-09/day-03/Redditv2/Redditv2/Services/IPostRepository.cs
@@ -0,0 +1,18 @@
+﻿using Redditv2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Redditv2.Services
+{
+    public interface IPostRepository
+    {
+        List<Post> GetPosts();
+        void AddPost(Post newPost);
+        void Upvote(Post votedPost);
+        void DownVote(Post votedPost);
+        Post GetPost(int id);
+
+    }
+}
---
+++ week-09/day-03/Redditv2/Redditv2/Services/PostRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Redditv2.Models;
+using Redditv2.Repositories;
+
+namespace Redditv2.Services
+{
+    public class PostRepository : IPostRepository
+    {
+        private RedditDbContext redditContext;
+
+        public PostRepository(RedditDbContext context)
+        {
+            redditContext = context;
+        }
+
+        public void AddPost(Post newPost)
+        {
+            redditContext.Posts.Add(newPost);
+            redditContext.SaveChanges();
+        }
+
+        public void DownVote(Post votedPost)
+        {
+            votedPost.Score--;
+            redditContext.SaveChanges();
+        }
+
+        public Post GetPost(int id)
+        {
+            return redditContext.Posts.Where(p => p.Id == id).SingleOrDefault();
+        }
+
+        public List<Post> GetPosts()
+        {
+            return redditContext.Posts.ToList();
+        }
+
+        public void Upvote(Post votedPost)
+        {
+            votedPost.Score++;
+            redditContext.SaveChanges();
+        }
+    }
+}
