a945e1f4c8b355185997bedc85b8fac8284667f1
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-12 11:48:30 +0200

    change (model): changing post model

---
+++ week-09/day-03/Redditv2/Redditv2/Migrations/20180712094708_PostModelChanged.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Redditv2.Migrations
+{
+    public partial class PostModelChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
