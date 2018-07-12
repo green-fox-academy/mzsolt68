63d5848b507c4146b243eff96ae5de5601f91bf6
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-12 12:04:16 +0200

    add (repository): adding User repository and interface

---
+++ week-09/day-03/Redditv2/Redditv2/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Redditv2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Redditv2.Repositories
+{
+    public interface IUserRepository
+    {
+        User GetUserById(int id);
+        User GetUserByName(string name);
+        void Adduser(User newUser);
+        void DeleteUser(User userToDelete);
+        void UpdateUser(User userToUpdate);
+    }
+}
---
+++ week-09/day-03/Redditv2/Redditv2/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Redditv2.Models;
+
+namespace Redditv2.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private RedditDbContext redditContext;
+
+        public UserRepository(RedditDbContext context)
+        {
+            redditContext = context;
+        }
+        public void Adduser(User newUser)
+        {
+            redditContext.Users.Add(newUser);
+            redditContext.SaveChanges();
+        }
+
+        public void DeleteUser(User userToDelete)
+        {
+            redditContext.Users.Remove(userToDelete);
+            redditContext.SaveChanges();
+        }
+
+        public User GetUserById(int id)
+        {
+            return redditContext.Users.Where(u => u.Id == id).SingleOrDefault();
+        }
+
+        public User GetUserByName(string name)
+        {
+            return redditContext.Users.Where(u => u.UserName == name).SingleOrDefault();
+        }
+
+        public void UpdateUser(User userToUpdate)
+        {
+            redditContext.Users.Update(userToUpdate);
+            redditContext.SaveChanges();
+        }
+    }
+}
