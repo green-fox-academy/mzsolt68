9db3d3775210126bb78c3d0cb923a7a0531b7a45
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-17 16:38:36 +0200

    add (method): adding box drawing code

--- week-02/day-04/Drawing/ColoredBox/MainWindow.xaml.cs
+++ week-02/day-04/Drawing/ColoredBox/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiddleLines;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,15 @@ namespace ColoredBox
         public MainWindow()
         {
             InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(100, 100, 200, 100);
+            foxDraw.StrokeColor(Colors.Black);
+            foxDraw.DrawLine(200, 100, 200, 200);
+            foxDraw.StrokeColor(Colors.Blue);
+            foxDraw.DrawLine(200, 200, 100, 200);
+            foxDraw.StrokeColor(Colors.Yellow);
+            foxDraw.DrawLine(100, 200, 100, 100);
         }
     }
 }
