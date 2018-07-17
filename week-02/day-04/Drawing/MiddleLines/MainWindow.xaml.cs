e1ce7292b1890c4f95a9d68682141f546c709d50
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-17 16:31:27 +0200

    modify (cleaning): removing unneeded codeline

--- week-02/day-04/Drawing/MiddleLines/MainWindow.xaml.cs
+++ week-02/day-04/Drawing/MiddleLines/MainWindow.xaml.cs
@@ -23,7 +23,6 @@ namespace MiddleLines
         public MainWindow()
         {
             InitializeComponent();
-            this.Title = $"{canvas.ActualHeight}, {canvas.ActualWidth}";
             var foxDraw = new FoxDraw(canvas);
             foxDraw.StrokeColor(Colors.Red);
             foxDraw.DrawLine(0, this.Height / 2, this.Width, this.Height / 2);
