a70a9e9740b4f0adecd189944d52801288a0f4bd
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-18 15:43:38 +0200

    add (feat): adding new drawing method EnvelopeStar

--- week-02/day-05/DrawTextures/DrawTextures/MainWindow.xaml.cs
+++ week-02/day-05/DrawTextures/DrawTextures/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,19 +22,39 @@ namespace DrawTextures
     public partial class MainWindow : Window
     {
         FoxDraw foxDraw;
+        int step;
         public MainWindow()
         {
             InitializeComponent();
             foxDraw = new FoxDraw(canvas);
             LinePlay();
-            Console.ReadKey();
-            canvas.Children.Clear();
+            EnvelopeStar();
+        }
 
+        private void EnvelopeStar()
+        {
+            step = 10;
+            Point start = new Point(0, this.Height / 2);
+            Point end = new Point(this.Width, this.Height / 2);
+            Point middle1 = new Point(this.Width / 2, this.Height / 2 - step);
+            Point middle2 = new Point(this.Width / 2, this.Height / 2 + step);
+            foxDraw.StrokeColor(Colors.Green);
+            while(start.X < this.Width / 2)
+            {
+                foxDraw.DrawLine(start, middle1);
+                foxDraw.DrawLine(middle1, end);
+                foxDraw.DrawLine(start, middle2);
+                foxDraw.DrawLine(middle2, end);
+                start.X += step;
+                end.X -= step;
+                middle1.Y -= step;
+                middle2.Y += step;
+            }
         }
 
         private void LinePlay()
         {
-            int step = 25;
+            step = 25;
             Point start1 = new Point(0, 0);
             Point end1 = new Point(this.Width, 0);
             Point start2 = new Point(0, 0);
