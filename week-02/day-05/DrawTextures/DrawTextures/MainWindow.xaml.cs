802589dccebe7f783fb5f1b3757deb099708424d
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-18 15:06:12 +0200

    add (feat): adding LinePlay method

--- week-02/day-05/DrawTextures/DrawTextures/MainWindow.xaml.cs
+++ week-02/day-05/DrawTextures/DrawTextures/MainWindow.xaml.cs
@@ -20,12 +20,35 @@ namespace DrawTextures
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        FoxDraw foxDraw;
         public MainWindow()
         {
             InitializeComponent();
+            foxDraw = new FoxDraw(canvas);
+            LinePlay();
+            Console.ReadKey();
+            canvas.Children.Clear();
+
         }
 
-
+        private void LinePlay()
+        {
+            int step = 25;
+            Point start1 = new Point(0, 0);
+            Point end1 = new Point(this.Width, 0);
+            Point start2 = new Point(0, 0);
+            Point end2 = new Point(0, this.Height);
+            while(start1.X < this.Width)
+            {
+                foxDraw.StrokeColor(Colors.Green);
+                foxDraw.DrawLine(start1, end1);
+                start1.X += step;
+                end1.Y += step;
+                foxDraw.StrokeColor(Colors.Purple);
+                foxDraw.DrawLine(start2, end2);
+                start2.Y += step;
+                end2.X += step;
+            }
+        }
     }
 }
