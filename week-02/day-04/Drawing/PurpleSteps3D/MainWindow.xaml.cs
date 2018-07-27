f1664f3abf0edeb385c77aa76e3ecfa47ef5ee99
Mlatilik Zsolt <mlatilik.zsolt@no-reply.github.com>
2018-07-27 14:41:14 +0200

    add (feature): add drawing method

--- week-02/day-04/Drawing/PurpleSteps3D/MainWindow.xaml.cs
+++ week-02/day-04/Drawing/PurpleSteps3D/MainWindow.xaml.cs
@@ -1,28 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MiddleLines;
 
 namespace PurpleSteps3D
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// Drawing 3D purple steps
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            FoxDraw foxDraw = new FoxDraw(canvas);
+            DrawSteps(foxDraw);
+        }
+
+        void DrawSteps(FoxDraw foxdraw)
+        {
+            foxdraw.StrokeColor(Colors.Black);
+            foxdraw.FillColor(Colors.Purple);
+            int size = 5;
+            double x = 0;
+            double y = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                foxdraw.DrawRectangle(x, y, size, size);
+                x += size;
+                y += size;
+                size *= 2;
+            }
         }
     }
 }
