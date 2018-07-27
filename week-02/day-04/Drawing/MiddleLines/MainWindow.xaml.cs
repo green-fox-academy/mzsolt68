c2bcca34390bb7be7c5dfdc1079a4497616d6631
Mlatilik Zsolt <mlatilik.zsolt@no-reply.github.com>
2018-07-27 14:23:44 +0200

    refactor (middlelines): move drawing commands to separate method

--- week-02/day-04/Drawing/MiddleLines/MainWindow.xaml.cs
+++ week-02/day-04/Drawing/MiddleLines/MainWindow.xaml.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MiddleLines
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// Draw lines in the middle of canvas
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -24,10 +13,15 @@ namespace MiddleLines
         {
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
-            foxDraw.StrokeColor(Colors.Red);
-            foxDraw.DrawLine(0, this.Height / 2, this.Width, this.Height / 2);
-            foxDraw.StrokeColor(Colors.Green);
-            foxDraw.DrawLine(this.Width / 2, 0, this.Width / 2, this.Height);
+            DrawLines(foxDraw);
+        }
+
+        void DrawLines(FoxDraw foxdraw)
+        {
+            foxdraw.StrokeColor(Colors.Red);
+            foxdraw.DrawLine(0, this.Height / 2, this.Width, this.Height / 2);
+            foxdraw.StrokeColor(Colors.Green);
+            foxdraw.DrawLine(this.Width / 2, 0, this.Width / 2, this.Height);
         }
     }
 }
