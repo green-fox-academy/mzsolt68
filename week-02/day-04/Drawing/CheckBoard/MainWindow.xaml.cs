b81f0a449df789cbbfe0c39a59279433e0f53f15
Mlatilik Zsolt <mlatilik.zsolt@no-reply.github.com>
2018-07-29 15:31:52 +0200

    add (feature): add checkboard drawing methods

--- week-02/day-04/Drawing/CheckBoard/MainWindow.xaml.cs
+++ week-02/day-04/Drawing/CheckBoard/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
+﻿using MiddleLines;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CheckBoard
 {
@@ -24,6 +15,45 @@ namespace CheckBoard
         public MainWindow()
         {
             InitializeComponent();
+            FoxDraw foxDraw = new FoxDraw(canvas);
+            DrawFrame(foxDraw);
+            DrawCheckBoard(foxDraw);
+        }
+
+        private void DrawFrame(FoxDraw foxDraw)
+        {
+            var points = new List<Point>
+            {
+                new Point(0,0),
+                new Point(400,0),
+                new Point(400,400),
+                new Point(0, 400)
+            };
+            foxDraw.StrokeColor(Colors.Black);
+            foxDraw.FillColor(Colors.White);
+            foxDraw.DrawPolygon(points);
+        }
+
+        private void DrawCheckBoard(FoxDraw foxDraw)
+        {
+            Point start = new Point(0, 0);
+            int size = 50;
+            //foxDraw.StrokeColor(Colors.Black);
+            foxDraw.FillColor(Colors.Black);
+            for(int row = 0; row < 8; row++)
+            {
+                for(int col = 0; col < 8; col++)
+                {
+                    if((row % 2 == 0) && (col % 2 == 0))
+                    {
+                        foxDraw.DrawRectangle(start.X + col * size, start.Y + row * size, size, size);
+                    }
+                    if ((row % 2 != 0) && (col % 2 != 0))
+                    {
+                        foxDraw.DrawRectangle(start.X + col * size, start.Y + row * size, size, size);
+                    }
+                }
+            }
         }
     }
 }
